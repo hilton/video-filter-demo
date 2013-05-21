baacc195b1086f1e1a3e482a813b23a3d2789535
Erik Bakker <erik.bakker@lunatech.com>
2013-05-21 11:29:05 +0200

    Use BufferedImage as chunks in the middle part of the pipeline

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -35,32 +35,36 @@ object Application extends Controller {
   val (rawStream, channel) = Concurrent.broadcast[Array[Byte]]
 
   // Allow to perform transformation
-  val video = new ImageEncoder
+  val transformer = new ImageTransformer
 
   // Enumeratees: an adapter from a stream of Froms to a stream of Tos. Note that an Enumeratee can rechunk differently,
   // add or remove chunks or parts of them. Enumeratees as we will see are instrumental for stream manipulation.
   // Here we are enumeratee that transform the image stream
-  val videoBlueEncoder = Enumeratee.map[Array[Byte]](video.color(_, Color.blue))
-  val videoPinkEncoder = Enumeratee.map[Array[Byte]](video.color(_, Color.pink))
-  val videoReflectionEncoder = Enumeratee.map[Array[Byte]](video.appendReflection)
-  val videoShadowEncoder = Enumeratee.map[Array[Byte]](video.appendShadow)
+  val readImage = Enumeratee.map[Array[Byte]](transformer.readImage)
+  val writeImage = Enumeratee.map[BufferedImage](transformer.writeImage)
+  val videoBlueEncoder = Enumeratee.map[BufferedImage](transformer.color(_, Color.blue))
+  val videoPinkEncoder = Enumeratee.map[BufferedImage](transformer.color(_, Color.pink))
+  val videoReflectionEncoder = Enumeratee.map[BufferedImage](transformer.appendReflection)
+  val videoShadowEncoder = Enumeratee.map[BufferedImage](transformer.appendShadow)
 
   // We apply transformation to our original image stream coming from the web cam
-  val videoStream = rawStream &> videoReflectionEncoder ><> videoPinkEncoder // ><> videoBlueEncoder ><> videoPinkEncoder
+  val videoStream = rawStream &> readImage ><> videoReflectionEncoder ><> videoPinkEncoder ><> writeImage
 
   // This is the ws method called from our main page
   def acquire = WebSocket.using[Array[Byte]] { implicit request =>
     // The Iteratee is coming from the camera. It is an immutable interface that represents a consumer, it consumes chunks of data each of type Byte[Array]
     // and eventually produces a computed value of type A. Iteratee[String,Int] is an iteratee that consumes chunks of strings and eventually
     // produces an Int (that could be for instance number of characters in the passed chunks)
-    (Iteratee.foreach[Array[Byte]] {
+    val iteratee = Iteratee.foreach[Array[Byte]] {
       case message: Array[Byte] => {
         // Push the message to the enumerator (rawstream)
         channel.push(message)
       }
     }.map { _ =>
       channel.eofAndEnd()
-    }, videoStream)
+    }
+
+    (iteratee, videoStream)
   }
 
   // This is the ws method called by our clients
@@ -69,7 +73,7 @@ object Application extends Controller {
   }
 
   // Class that transform an image into another image
-  case class ImageEncoder() {
+  class ImageTransformer {
 
     val renderer = new ReflectionRenderer
     renderer.setOpacity(0.5f)
@@ -79,46 +83,31 @@ object Application extends Controller {
     val shadow = new org.jdesktop.swingx.graphics.ShadowRenderer
     shadow.setOpacity(0.4f)
 
-    def color(data: Array[Byte], color: Color): Array[Byte] = {
-      val filter = new org.jdesktop.swingx.image.ColorTintFilter(color, 0.4f)
-      val stream = new ByteArrayInputStream(data)
+    def readImage(byteArray: Array[Byte]) = {
+      val stream = new ByteArrayInputStream(byteArray)
       val image = ImageIO.read(stream)
+      image
+    }
+
+    def writeImage(image: BufferedImage) = {
+      val baos = new ByteArrayOutputStream()
+      ImageIO.write(image, "png", baos)
+      baos.flush()
+      baos.toByteArray
+    }
+
+    def color(image: BufferedImage, color: Color): BufferedImage = {
+      val filter = new org.jdesktop.swingx.image.ColorTintFilter(color, 0.4f)
       val dst = new BufferedImage(image.getWidth, image.getHeight, image.getType)
       filter.filter(image, dst)
-
-      val baos = new ByteArrayOutputStream()
-      ImageIO.write(dst, "png", baos)
-      baos.flush()
-
-      baos.toByteArray
+      dst
     }
 
-    def appendReflection(data: Array[Byte]): Array[Byte] = {
-      val stream = new ByteArrayInputStream(data)
-      val image = ImageIO.read(stream)
+    def appendReflection(image: BufferedImage): BufferedImage =
+      renderer.appendReflection(image)
 
-      val reflection = renderer.appendReflection(image)
-
-      val baos = new ByteArrayOutputStream()
-      ImageIO.write(reflection, "png", baos)
-      baos.flush()
-
-      baos.toByteArray
-
-    }
-
-    def appendShadow(data: Array[Byte]): Array[Byte] = {
-      val stream = new ByteArrayInputStream(data)
-      val image = ImageIO.read(stream)
-
-      val reflection = shadow.createShadow(image)
-
-      val baos = new ByteArrayOutputStream()
-      ImageIO.write(reflection, "png", baos)
-      baos.flush()
-
-      baos.toByteArray
-    }
+    def appendShadow(image: BufferedImage): BufferedImage =
+      shadow.createShadow(image)
 
   }
 
